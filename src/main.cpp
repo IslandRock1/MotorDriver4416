381d1dbc401076ebfdd5ff2202ca468a1ed3b107
Øystein Bringsli <oysteinmb@gmail.com>
2025-05-11 14:32:04 +0200

    Idk

--- src/main.cpp
+++ src/main.cpp
@@ -34,19 +34,18 @@ void setup() {
 
 void loop() {
 
-    // if (!hasInitialized && (initTime + 5000 < millis())) {
+    if (!hasInitialized && (initTime + 5000 < millis())) {
 
-    //     initTime = millis();
-    //     motorController.incrementPositionRounds(1.0);
-    // }
+        initTime = millis();
+        motorController.incrementPositionRounds(1.0);
+    }
 
     if (lastPrint + 500 < millis()) {
         PrintInfo::printRPM(motorController.desiredPosition);
-        PrintInfo::printDriverTime(MotorController::sumTime);
+        // PrintInfo::printDriverTime(MotorController::sumTime);
         MotorController::sumTime = 0;
         lastPrint = millis();
     }
 
-    // motorController.updatePosition();
-    motorController.controlMotorSpeed(255, false);
+    motorController.updatePosition();
 }
